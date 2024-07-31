0075da551ab6ae5f5cb926f626e644131a4a3378
DeeptiEmmi <emmideepti21@gmail.com>
2024-07-31 21:28:02 +0530

    Update reset.cpp

--- reset.cpp
+++ reset.cpp
@@ -1,3 +1,5 @@
+//Deepti Emmi
+//23070123049
 #include<iostream>
 using namespace std;
 int main(){
@@ -10,3 +12,8 @@ x=00000001<<reset;
 output= num&(~x);
 cout<<"Set number is "<<output ;
 }
+/* Output-
+ * cout<<"Enter the number to reset5
+Enter the bit to set2
+Set number is5 
+ */
