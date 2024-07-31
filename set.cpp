1f56b3d49d6d833d372020ab1ad0c7f765940276
DeeptiEmmi <emmideepti21@gmail.com>
2024-07-31 21:30:18 +0530

    Update set.cpp

--- set.cpp
+++ set.cpp
@@ -1,3 +1,5 @@
+//Deepti Emmi
+//23070123049
 #include<iostream>
 using namespace std;
 int main(){
@@ -10,3 +12,10 @@ x=00000001<<set;
 output= num|x;
 cout<<"Set number is "<<output ;
 }
+
+//Enter the number to set5
+//Enter the bit to set6
+//Set number is 69
+
+//=== Code Execution Successful === 
+//
